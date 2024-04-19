38fc211f5fb319fdf6e7d86a92de4d2d2a6a6d29
SHAIK ABDULHAMEED <shaikabdulhameedd@gmail.com>
2024-04-19 18:16:45 +0530

    making responses good

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const  JWT_SECRET ="SECRET"
+const JWT_SECRET = "SECRET";
 
 // Middleware function to authenticate requests
 const authMiddleware = (req, res, next) => {
@@ -8,21 +8,23 @@ const authMiddleware = (req, res, next) => {
   console.log(authHeader);
 
   // Check if authorization header is present and starts with 'Bearer '
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-      return res.status(403).json({}); // Return 403 Forbidden status if authentication fails
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(403).json({
+      msg: "There should be JWT token in header before accessing Quiz routes you can get it by going /users/signup in body give username and password then in response you get token put that in Bearer token in every quiz route FORMAT is Authorization as header key and value Bearer <token>",
+    }); // Return 403 Forbidden status if authentication fails
   }
 
-  const token = authHeader.split(' ')[1]; // Extract the token from the authorization header
+  const token = authHeader.split(" ")[1]; // Extract the token from the authorization header
   try {
-      const decoded = jwt.verify(token, JWT_SECRET); // Verify the token using the secret key
+    const decoded = jwt.verify(token, JWT_SECRET); // Verify the token using the secret key
 
-      req.userId = decoded.userId; // Attach the decoded user ID to the request object
-      next(); // Call the next middleware or route handler
+    req.userId = decoded.userId; // Attach the decoded user ID to the request object
+    next(); // Call the next middleware or route handler
   } catch (err) {
-      return res.status(403).json({"message":"Middleware error", err}); // Return 403 Forbidden status with error message if verification fails
+    return res.status(403).json({ message: "Middleware error", err }); // Return 403 Forbidden status with error message if verification fails
   }
 };
 
 module.exports = {
-  authMiddleware
-}
+  authMiddleware,
+};
