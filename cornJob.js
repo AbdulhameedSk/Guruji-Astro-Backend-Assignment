7a1cc9888d57ba8c85059221eea898468410309d
SHAIK ABDULHAMEED <shaikabdulhameedd@gmail.com>
2024-04-19 17:51:44 +0530

    Corn job completed

--- cornJob.js
+++ cornJob.js
@@ -1,7 +1,34 @@
 const cron = require('node-cron');
+const MongoClient = require('mongodb').MongoClient;
 
-cron.schedule('0 0 * * *', () => {
-  // Your task to be executed at midnight
-  console.log('Running the task at midnight!');
-  // Add your backend task logic here
+const uri = process.env.MONGO_URI || 'mongodb+srv://abdulhameed:abdulhameed@travelweb.upbqs02.mongodb.net/quiz'
+const client = new MongoClient(uri);
+
+cron.schedule('0 0 * * *', async () => {
+  console.log('CORN Running!');
+
+  try {
+    // Connect to the MongoDB client
+    await client.connect();
+
+    // Get the source and destination collections
+    const sourceCollection = client.db('quiz').collection('quizzes');
+    const destinationCollection = client.db('quiz').collection('quizzesBackup');
+
+    // Fetch all documents from the source collection
+    const quizzesBack = await sourceCollection.find().toArray();
+
+    // If there are quizzes, insert them into the destination collection
+    if (quizzesBack.length > 0) {
+      await destinationCollection.insertMany(quizzesBack);
+      console.log('Database dump successful!');
+    } else {
+      console.log('No quizzes to backup');
+    }
+  } catch (error) {
+    console.error(`Error executing database dump: ${error}`);
+  } finally {
+    // Close the MongoDB client
+    await client.close();
+  }
 });
